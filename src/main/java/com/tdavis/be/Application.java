bdd2d8202ea4818fe5d0756bdd4f70aabdc00d18
lucky <lucky@Luckybob>
2016-09-30 21:12:50 -0500

    Fixed Spring Security configuration.  Removed security.xml and used spring annotations. Applied data check on DB to prevent duplication errors. Tested on pcf successfully.

--- src/main/java/com/tdavis/be/Application.java
+++ src/main/java/com/tdavis/be/Application.java
@@ -2,9 +2,8 @@ package com.tdavis.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
-@ImportResource("classpath:security.xml")
+
 @SpringBootApplication
 public class Application {
 
--- src/main/java/com/tdavis/be/configuration/UserConfiguration.java
+++ src/main/java/com/tdavis/be/configuration/UserConfiguration.java
@@ -1,15 +1,62 @@
 package com.tdavis.be.configuration;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableAutoConfiguration
-@EntityScan(basePackages="com.tdavis.be")
-@EnableTransactionManagement
-public class UserConfiguration {
-	
-	
+public class UserConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		// @formatter:off
+		auth
+			.jdbcAuthentication()
+				.dataSource(dataSource)
+				.authoritiesByUsernameQuery(
+					"select app_user.name, role.name from app_user" +
+					" join app_user_role on app_user.id = app_user_role.users_id" +
+					" join role on app_user_role.roles_id = role.id" +
+					" where app_user.name = ?")
+				.usersByUsernameQuery("select name,password,enabled from app_user where name = ?")
+				.passwordEncoder(new BCryptPasswordEncoder());
+		// @formatter:on
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.csrf()
+				.disable()
+			.authorizeRequests()
+				.antMatchers("/resources/**", "/vendors/**", "/build/**", "/js/**").permitAll()
+				.antMatchers("/admin/**").hasRole("ADMIN")
+				.anyRequest().hasRole("USER")
+				.and()
+			.formLogin()
+				.loginPage("/login.html")
+				.usernameParameter("j_username")
+				.passwordParameter("j_password")
+				.loginProcessingUrl("/j_spring_security_check")
+				.defaultSuccessUrl("/")
+				.permitAll()
+				.and()
+			.logout()
+				/* @see: org.springframework.security.config.annotation.web.configurers.LogoutConfigurer#logoutUrl */
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+				.and()
+			.rememberMe();
+		// @formatter:on
+	}
+
 }
